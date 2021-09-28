641ef9ffd5c3bbe8714d3331e64cf545299b432e
AC-Dc <changdavidson4@gmail.com>
2021-09-28 00:07:32 -0400

    re-add index.ts

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import Client from '../classes/Client';
+import 'dotenv/config.js';
+
+const { APPLICATION_ID, PUBLIC_KEY, TOKEN, PORT } = process.env;
+
+const client = new Client({
+  port: Number(PORT),
+  token: TOKEN,
+  applicationId: APPLICATION_ID,
+  publicKey: PUBLIC_KEY,
+});
+
+client.start();
