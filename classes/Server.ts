b935a2940bdea599a897f92592cb42b638cf4c25
Coding <dev.tandpfun@gmail.com>
2021-09-28 13:31:00 -0700

    Add API for getting questions

--- classes/Server.ts
+++ classes/Server.ts
@@ -16,6 +16,34 @@ export default class Server {
     this.router.post('/interactions', verifyKeyMiddleware(this.client.publicKey), (req, res) =>
       this.handleRequest(req, res)
     );
+
+    this.router.get('/api/questions/:questionType', (req, res) => {
+      const questionType = req.params.questionType;
+      const rating = req.query.rating;
+      if (!['dare', 'truth', 'nhie', 'wyr'].includes(questionType as string))
+        return res
+          .send({
+            error: true,
+            message: 'The question type must be one of the following: "dare" "truth" "nhie" "wyr"',
+          })
+          .status(400);
+      if (!rating)
+        return res.send({
+          question: this.client.randomQuestion(questionType as 'dare' | 'truth' | 'nhie' | 'wyr'),
+        });
+      if (!['pg', 'pg13', 'r'].includes(rating as string))
+        return res
+          .send({
+            error: true,
+            message: 'The rating must be one of the following: "pg" "pg13" "r"',
+          })
+          .status(400);
+      res.send({
+        question: this.client.randomQuestion(questionType as 'dare' | 'truth' | 'nhie' | 'wyr', [
+          rating as 'pg' | 'pg13' | 'r',
+        ]),
+      });
+    });
   }
 
   start() {
