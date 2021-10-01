fabacc41485900d8f358bcb5d61acaa42a68fc9e
AC-Dc <changdavidson4@gmail.com>
2021-09-30 21:41:00 -0400

    permissions fix

--- src/classes/Functions.ts
+++ src/classes/Functions.ts
@@ -6,7 +6,7 @@ import Context from './Context';
 export function checkPerms(command: Command, ctx: Context) {
   const required = command.perms
     .map(perm => (typeof perm === 'bigint' ? perm : PermissionFlagsBits[perm]))
-    .reduce((a, c) => a & c, 0n);
+    .reduce((a, c) => a | c, 0n);
 
   if ((BigInt(ctx.member.permissions) & required) !== required) {
     ctx.reply(`${Client.EMOTES.xmark} you need more perms`); // TODO: better message
