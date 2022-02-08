8d950383eb24fe99ca63f1f514e679e9504404b4
tandpfun <dev.tandpfun@gmail.com>
2022-02-07 21:10:16 -0800

    random command

---
+++ src/commands/random.ts
@@ -0,0 +1,55 @@
+import { ApplicationCommandOptionType } from 'discord-api-types';
+import { QuestionType } from '@prisma/client';
+
+import type { Mutable } from '../classes/OptionTypes';
+import type Command from '../classes/Command';
+import type Context from '../classes/Context';
+
+const options = [
+  {
+    type: ApplicationCommandOptionType.String,
+    name: 'rating',
+    description: 'The maturity level of the topics the question can relate to.',
+    choices: [
+      { name: 'PG', value: 'PG' },
+      { name: 'PG13', value: 'PG13' },
+      { name: 'R', value: 'R' },
+    ],
+  },
+] as const;
+
+const tod: Command = {
+  name: 'random',
+  description: 'Gives a random question from any category (truth, dare, paranoia, wyr, nhie).',
+  category: 'question',
+  options,
+  perms: [],
+  run: async (ctx: Context): Promise<void> => {
+    const channelSettings = await ctx.channelSettings;
+    const types = Object.values(QuestionType);
+    const type = types[Math.floor(Math.random() * types.length)];
+    const rating = ctx.getOption<Mutable<typeof options[0]>>('rating')?.value;
+    const result = await ctx.client.database.getRandomQuestion(
+      type,
+      channelSettings.disabledRatings,
+      rating,
+      ctx.guildId
+    );
+    ctx.reply({
+      content: ctx.client.functions.upvoteAd(),
+      embeds: [
+        {
+          title: result.question,
+          color: ctx.client.COLORS.BLUE,
+          footer: result.id
+            ? {
+                text: `Type: ${result.type} | Rating: ${result.rating} | ID: ${result.id}`,
+              }
+            : undefined,
+        },
+      ],
+    });
+  },
+};
+
+export default tod;
