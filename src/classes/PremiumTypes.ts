335570d400f2f341f3cb3667151540881aec1750
Coding <28990589+tandpfun@users.noreply.github.com>
2022-10-14 17:47:33 -0700

    feat: add function for checking entitlement

--- src/classes/PremiumTypes.ts
+++ src/classes/PremiumTypes.ts
@@ -9,3 +9,21 @@ export type APIChatInputApplicationCommandInteractionWithEntitlements =
 export type APIMessageComponentInteractionWithEntitlements = APIMessageComponentInteraction & {
   entitlement_sku_ids?: string[];
 };
+
+export type APIApplicationEntitlement = {
+  id: string;
+  sku_id: string;
+  application_id: string;
+  user_id: string;
+  promotion_id?: string;
+  type: number;
+  deleted: boolean;
+  gift_code_flags: number;
+  consumed: boolean;
+  starts_at: string;
+  ends_at: string;
+  guild_id: string;
+  subscription_id: string;
+};
+
+export type RESTGetAPIApplicationEntitlementsResult = APIApplicationEntitlement[];
